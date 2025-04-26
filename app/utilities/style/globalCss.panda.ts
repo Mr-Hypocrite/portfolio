797b511c4c703250790382817c84b86ef338250c
Jatin <ym.jatin@gmail.com>
2025-04-26 13:52:09 +0530

    chore: rename file and add smooth-scrolling

--- app/utilities/style/globalCss.panda.ts
+++ app/utilities/style/globalCss.panda.ts
@@ -1,6 +1,9 @@
 import { defineGlobalStyles } from '@pandacss/dev';
 
 export const globalCss = defineGlobalStyles({
+    html: {
+        scrollBehavior: 'smooth'
+    },
     body: {
         backgroundColor: 'var(--colors-bg-canvas)',
         color: 'var(--colors-fg-default)'
