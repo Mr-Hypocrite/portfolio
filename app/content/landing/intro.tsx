b68c054c1b541acd4cb0b985e24bc2a4291a2649
Jatin <ym.jatin@gmail.com>
2025-05-05 13:46:07 +0530

    wip: update intro content

--- app/content/landing/intro.tsx
+++ app/content/landing/intro.tsx
@@ -1,10 +1,39 @@
-import { Section, Text } from '@app/components';
+import { Icon, Section, Text } from '@app/components';
+import { ChevronDownIcon } from 'lucide-react';
+import Link from 'next/link';
 import { FC } from 'react';
+import { Flex } from 'styled-system/jsx';
 
 export const Intro: FC = () => {
     return (
-        <Section>
-            <Text>intro</Text>
+        <Section id="home" flexDir={'column'} textAlign={'center'} position={'relative'}>
+            <Text as={'h2'} textStyle={'heading'}>
+                hi. i'm jatin.
+            </Text>
+            <Text as={'h2'} textStyle={'heading'} lineHeight={'none'}>
+                a {`<web />`} developer.
+            </Text>
+            <Text textStyle={'sub-heading'} mt={'8'}>
+                on a journey to build better web experiences.
+            </Text>
+
+            <Link href="/#work">
+                <Flex
+                    alignItems={'center'}
+                    direction={'column'}
+                    position={'absolute'}
+                    bottom={'16'}
+                    left={'0'}
+                    right={'0'}
+                    mx={'auto'}
+                    w={'fit'}
+                >
+                    <Text textStyle={'sub-heading'}>work</Text>
+                    <Icon size={'2xl'}>
+                        <ChevronDownIcon />
+                    </Icon>
+                </Flex>
+            </Link>
         </Section>
     );
 };
