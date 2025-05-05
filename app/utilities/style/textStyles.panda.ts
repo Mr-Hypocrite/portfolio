aab94eeb072ac8077ff8e9ffafb52cb318b4d388
Jatin <ym.jatin@gmail.com>
2025-05-05 13:45:50 +0530

    chore: update textStyles

--- app/utilities/style/textStyles.panda.ts
+++ app/utilities/style/textStyles.panda.ts
@@ -2,10 +2,23 @@ import { defineTextStyles } from '@pandacss/dev';
 
 export const textStyles = defineTextStyles({
     body: {
-        description: '',
+        description: 'for body text',
         value: {}
     },
-
+    heading: {
+        description: 'for headings',
+        value: {
+            fontFamily: 'poppins',
+            fontSize: { base: '2xl', md: '4xl' },
+            fontWeight: { base: 'medium' }
+        }
+    },
+    'sub-heading': {
+        value: {
+            fontSize: { base: 'md', md: 'xl' },
+            fontWeight: { base: 'normal' }
+        }
+    },
     'header-link': {
         description: '',
         value: {}
