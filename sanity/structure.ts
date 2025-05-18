2604526a60ad07d5dafe60f6e3737522768ee5fa
Jatin <ym.jatin@gmail.com>
2025-05-18 17:05:58 +0530

    chore: cms structure
    define singleton docs where it's required

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,7 +1,29 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from 'sanity/structure';
+
+// Define the actions that should be available for singleton documents
+const singletonActions = new Set(['publish', 'discardChanges', 'restore']);
+
+// Define the singleton document types
+const singletonTypes = new Set(['personalDetails']);
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
-  S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    S.list()
+        .title('Content')
+        .items([
+            S.listItem().title('Personal Details').id('personal-details').child(
+                // Instead of rendering a list of documents, we render a single
+                // document, specifying the `documentId` manually to ensure
+                // that we're editing the single instance of the document
+                S.editor()
+                    .id('personal-details-editor')
+                    .schemaType('personalDetails')
+                    .documentId('personal-details') // Ensures only one doc
+            ),
+            // Divider
+            S.divider(),
+            ...S.documentTypeListItems().filter((listItem) => {
+                const id = listItem.getId();
+                return id ? !singletonTypes.has(id) : true;
+            })
+        ]);
