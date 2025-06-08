d20b5db8fdf1415353b41c2c98efeec6615eb3ee
Jatin <ym.jatin@gmail.com>
2025-06-09 01:14:39 +0530

    chore: add config for singleton docs

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,25 +1,61 @@
 import type { StructureResolver } from 'sanity/structure';
 
-// Define the actions that should be available for singleton documents
-const singletonActions = new Set(['publish', 'discardChanges', 'restore']);
-
 // Define the singleton document types
-const singletonTypes = new Set(['personalDetails']);
+const singletonTypes = new Set([
+    'personalDetails',
+    'projectPage',
+    'homePage',
+    'aboutPage',
+    'workSequence'
+]);
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
     S.list()
         .title('Content')
         .items([
-            S.listItem().title('Personal Details').id('personal-details').child(
+            S.listItem().title('Home Page').id('home-page').child(
                 // Instead of rendering a list of documents, we render a single
                 // document, specifying the `documentId` manually to ensure
                 // that we're editing the single instance of the document
-                S.editor()
-                    .id('personal-details-editor')
-                    .schemaType('personalDetails')
-                    .documentId('personal-details') // Ensures only one doc
+                S.editor().id('home-page-editor').schemaType('homePage').documentId('home-page') // Ensures only one doc
             ),
+            S.listItem()
+                .title('About Page')
+                .id('about-page')
+                .child(
+                    S.editor()
+                        .id('about-page-editor')
+                        .schemaType('aboutPage')
+                        .documentId('about-page')
+                ),
+            S.listItem()
+                .title('Personal Details')
+                .id('personal-details')
+                .child(
+                    S.editor()
+                        .id('personal-details-editor')
+                        .schemaType('personalDetails')
+                        .documentId('personal-details')
+                ),
+            S.listItem()
+                .title('Project Page')
+                .id('project-page')
+                .child(
+                    S.editor()
+                        .id('personal-page-editor')
+                        .schemaType('projectPage')
+                        .documentId('project-page')
+                ),
+            S.listItem()
+                .title('Work Sequence')
+                .id('work-sequence')
+                .child(
+                    S.editor()
+                        .id('work-sequence-editor')
+                        .schemaType('workSequence')
+                        .documentId('work-sequence')
+                ),
             // Divider
             S.divider(),
             ...S.documentTypeListItems().filter((listItem) => {
