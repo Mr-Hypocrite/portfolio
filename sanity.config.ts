75fe7a3ae924f4555fd38b32273bf1a9dc097d06
Jatin <ym.jatin@gmail.com>
2025-06-09 01:20:18 +0530

    chore: update sanity config add seo plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -12,6 +12,7 @@ import { structureTool } from 'sanity/structure';
 import { apiVersion, dataset, projectId } from './sanity/env';
 import { schema } from './sanity/schemaTypes';
 import { structure } from './sanity/structure';
+import { seoMetaFields } from 'sanity-plugin-seo';
 
 export default defineConfig({
     basePath: '/studio',
@@ -25,6 +26,7 @@ export default defineConfig({
         }),
         // Vision is for querying with GROQ from inside the Studio
         // https://www.sanity.io/docs/the-vision-plugin
-        visionTool({ defaultApiVersion: apiVersion })
+        visionTool({ defaultApiVersion: apiVersion }),
+        seoMetaFields()
     ]
 });
