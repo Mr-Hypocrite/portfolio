5ba139b52c773bf9a8d660df5db3cdab7521bf37
Jatin <ym.jatin@gmail.com>
2025-06-09 01:19:20 +0530

    refactor: use image builder to fetch optimized images from sanity

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,11 +1,22 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import createImageUrlBuilder from '@sanity/image-url';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { dataset, projectId } from '../env';
 
-import { dataset, projectId } from '../env'
+const builder = createImageUrlBuilder({ projectId, dataset });
 
-// https://www.sanity.io/docs/image-url
-const builder = createImageUrlBuilder({ projectId, dataset })
-
-export const urlFor = (source: SanityImageSource) => {
-  return builder.image(source)
-}
+export const sanityLoader = ({
+    src,
+    width,
+    quality
+}: {
+    src: SanityImageSource;
+    width: number;
+    quality?: number;
+}) => {
+    return builder
+        .image(src)
+        .width(width)
+        .quality(quality || 75)
+        .auto('format')
+        .url();
+};
