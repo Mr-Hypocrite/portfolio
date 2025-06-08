d308eb8d1587cf9eb727e66da960ededaddc00e7
Jatin <ym.jatin@gmail.com>
2025-06-09 01:22:26 +0530

    chore: add default seo content
    dynamic, fetched from sanity

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,9 @@
+import { Footer, Header, ScrollRestoration } from '@/components';
 import type { Metadata } from 'next';
-import { Container } from 'styled-system/jsx';
-import { Footer, Header, ScrollRestoration } from '@app/components';
 import { Inter, Poppins } from 'next/font/google';
+import { Container } from 'styled-system/jsx';
 import './index.css';
+import { getPageSeo } from '@/sanity/getters';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const poppins = Poppins({
@@ -11,43 +12,51 @@ const poppins = Poppins({
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 });
 
-//TODO: placeholders for now will have to update later
-export const metadata: Metadata = {
-    title: 'Jatin | Web Developer Portfolio',
-    description:
-        'Jatin is a web developer on a journey to build better web experiences. With over 2 years of hands-on experience, explore projects, skills, and more.',
-    metadataBase: new URL('https://yourdomain.com'), // Update with your actual domain
-    openGraph: {
-        title: 'Jatin | Web Developer Portfolio',
+export async function generateMetadata(): Promise<Metadata> {
+    const pageSeo = await getPageSeo('home-page');
+
+    return {
+        title: pageSeo.metaTitle || 'Jatin | Web Developer Portfolio',
         description:
-            'On a journey to build better web experiences. With 2+ years of real-world web development experience, check out my projects and story.',
-        url: 'https://yourdomain.com',
-        siteName: 'Jatin Portfolio',
-        images: [
-            {
-                url: '/og-image.png',
-                width: 1200,
-                height: 630,
-                alt: "Jatin's Portfolio Preview"
-            }
-        ],
-        type: 'website'
-    },
-    twitter: {
-        card: 'summary_large_image',
-        title: 'Jatin | Web Developer Portfolio',
-        description:
-            'On a journey to build better web experiences. 2+ years of coding and creating in the web dev world.',
-        images: ['/twitter-image.png']
-    },
-    authors: [{ name: 'Jatin' }],
-    robots: {
-        index: true,
-        follow: true,
-        nocache: false
-    },
-    creator: 'Jatin'
-};
+            pageSeo.metaDescription ||
+            'Jatin is a web developer on a journey to build better web experiences. With over 2 years of hands-on experience, explore projects, skills, and more.',
+        metadataBase: new URL('https://yourdomain.com'), // Update with your actual domain
+        openGraph: {
+            title: pageSeo.openGraph?.title || 'Jatin | Web Developer Portfolio',
+            description:
+                pageSeo.openGraph?.description ||
+                'On a journey to build better web experiences. With 2+ years of real-world web development experience, check out my projects and story.',
+            url: pageSeo.openGraph?.url || 'https://yourdomain.com',
+            siteName: pageSeo.openGraph?.siteName || 'Jatin',
+            images: [
+                {
+                    url: pageSeo?.openGraph?.image?.asset?.url || '/og-image.png',
+                    width: 1200,
+                    height: 630,
+                    alt: "Jatin's Portfolio Preview"
+                }
+            ],
+            type: 'website'
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: 'Jatin | Web Developer Portfolio',
+            description:
+                'On a journey to build better web experiences. 2+ years of coding and creating in the web dev world.',
+            images: ['/twitter-image.png']
+        },
+        authors: [{ name: 'Jatin' }],
+        robots: {
+            index: true,
+            follow: true,
+            nocache: false
+        },
+        creator: 'Jatin',
+        icons: {
+            icon: '/favicon/dark/favicon.ico'
+        }
+    };
+}
 
 export default function RootLayout({
     children
@@ -59,7 +68,7 @@ export default function RootLayout({
             <body className={'dark'}>
                 <Header />
                 <ScrollRestoration />
-                <Container>{children}</Container>
+                <Container pt={'24'}>{children}</Container>
                 <Footer />
             </body>
         </html>
